7f94d5d39023c6ff29a085a3cd2297028c4b5852
Yinqamster <1052317189@qq.com>
2019-10-04 21:55:02 -0400

    update

--- src/Person.java
+++ src/Person.java
@@ -1,14 +1,14 @@
 
 public class Person {
-	private String name;
+	private Name name;
 	
 	public Person() {}
 	public Person(String name) {
-		this.name = name;
+		this.name.setNickName(name);
 	}
 	
 	public String getName() {
-		return this.name;
+		return this.name.getNickName();
 	}
 	
 	public void printName(){
